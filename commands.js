97c6925c5610580c94f34d9775eda8ebfa169556
Zach Covington <zachcovi@gmail.com>
2023-01-18 20:19:03 -0600

    Update command handling - Removed game.js - Added logic to make calling sub commands easier.

--- commands.js
+++ commands.js
@@ -1,5 +1,5 @@
 import { getRPSChoices } from './game.js';
-import { getPZChoices } from './spiffo.js';
+import { getPZChoicesList, getPZChoices } from './spiffo.js';
 import { VerifyDiscordRequest, getRandomEmoji, DiscordRequest, capitalize } from './utils.js';
 import { getShuffledOptions, getResult } from './game.js';
 import {
@@ -9,7 +9,6 @@ import {
   MessageComponentTypes,
   ButtonStyleTypes,
 } from 'discord-interactions';
-import { exec }from 'node:child_process';
 export async function HasGuildCommands(appId, guildId) {
   if (guildId === '' || appId === '') return;
   // API endpoint to get and post guild commands
@@ -22,6 +21,7 @@ export async function HasGuildCommands(appId, guildId) {
     if (data) {
       const installedNames = data.map((c) => c['name']);
       const toInstall = cmdList.map((c) => c['name'])
+      console.log(toInstall)
       installedNames.forEach(function(command) {
         // If command is installed on server but not in commands.js, delete it from the server
         if (!toInstall.includes(command)) {
@@ -36,7 +36,12 @@ export async function HasGuildCommands(appId, guildId) {
         } else {
           console.log(`Installing "${command}"`);
           InstallGuildCommand(appId, guildId, command);
-          
+        }
+      });
+      toInstall.forEach(function(command) {
+        if (!installedNames.includes(command)) {
+          console.log('Need to install '+command)
+          InstallGuildCommand(appId, guildId, command);
         }
       });
       
@@ -86,8 +91,9 @@ export async function InstallGuildCommand(appId, guildId, command) {
   // API endpoint to get and post guild commands
   const endpoint = `applications/${appId}/guilds/${guildId}/commands`;
   // install command
+  var cmdObj = cmdList.find(c => c.name == command)
   try {
-    await DiscordRequest(endpoint, { method: 'POST', body: command });
+    await DiscordRequest(endpoint, { method: 'POST', body: cmdObj });
   } catch (err) {
     console.error(err);
   }
@@ -95,7 +101,7 @@ export async function InstallGuildCommand(appId, guildId, command) {
 
 // Get the game choices from game.js
 function createPZChoices() {
-  const choices = getPZChoices();
+  const choices = getPZChoicesList();
   const commandChoices = [];
 
   for (let choice of choices) {
@@ -125,8 +131,8 @@ function createCommandChoices() {
 
 export const cmdList = [
   {
-    name: 'test1',
-    description: 'Basic guild command',
+    name: 'hello_world',
+    description: 'Hello World Sanity Test',
     func: function(req, res, id) {
       return res.send({
         type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
@@ -139,89 +145,32 @@ export const cmdList = [
     type: 1,
   },
   {
-    name: 'pzserver',
-    description: 'Manage the servers Project Zomboid Server Container',
-    func: function(req, res, id) {
-      const userId = req.body.member.user.id;
+    name: 'server',
+    description: 'Manage the Project Zomboid Server',
+    func: async function(req, res, id) {
+      
       // User's object choice
-      const objectName = req.body.data.options[0].value;
-      console.log("objectName: "+objectName)
-      exec('docker restart zomboid-dedicated-server',(err, output) => {
-        // once the command has completed, the callback function is called
-        if (err) {
-          // log and return if we encounter an error
-          console.error("could not execute command: ", err)
-          return
-        }
-        // log the output received from the command
-        console.log("Output: \n", output) 
-      })
+      const cmdName = req.body.data.options[0].value;
+      console.log("cmdName: "+cmdName)
+      var pzCmds = getPZChoices()
+      var message = await pzCmds[cmdName].func(req,res,id)
       return res.send({
       type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
       data: {
           // Fetches a random emoji to send from a helper function
-          content: `SERVER RESTART INITIATED BY <@${userId}>`,
+          content: message,
       },
       });
   },
     options: [
       {
         type: 3,
-        name: 'object',
-        description: 'Pick an action',
+        name: 'command',
+        description: 'Pick a Server Command',
         required: true,
         choices: createPZChoices(), 
       },
     ],
     type: 1,
   },
-  // {
-  //   name: 'challenge',
-  //   description: 'Challenge to a match of rock paper scissors',
-  //   func: function(req,id){
-  //       {
-  //         const userId = req.body.member.user.id;
-  //         // User's object choice
-  //         const objectName = req.body.data.options[0].value;
-
-  //         // Create active game using message ID as the game ID
-  //         activeGames[id] = {
-  //             id: userId,
-  //             objectName,
-  //         };
-
-  //         return res.send({
-  //         type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
-  //         data: {
-  //             // Fetches a random emoji to send from a helper function
-  //             content: `Rock papers scissors challenge from <@${userId}>`,
-  //             components: [
-  //             {
-  //                 type: MessageComponentTypes.ACTION_ROW,
-  //                 components: [
-  //                 {
-  //                     type: MessageComponentTypes.BUTTON,
-  //                     // Append the game ID to use later on
-  //                     custom_id: `accept_button_${req.body.id}`,
-  //                     label: 'Accept',
-  //                     style: ButtonStyleTypes.PRIMARY,
-  //                 },
-  //                 ],
-  //             },
-  //             ],
-  //         },
-  //         });
-  //     }
-  //   },
-  //   options: [
-  //     {
-  //       type: 3,
-  //       name: 'object',
-  //       description: 'Pick your object',
-  //       required: true,
-  //       choices: createCommandChoices(),
-  //     },
-  //   ],
-  //   type: 1,
-  // }
 ]
