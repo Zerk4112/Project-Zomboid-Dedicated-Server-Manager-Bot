c8302207e3b30d2d8184a75c33cf534e97dd16d4
Zach Covington <zachcovi@gmail.com>
2023-01-18 20:15:37 -0600

    Remove game.js from app.js

--- app.js
+++ app.js
@@ -22,7 +22,6 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json({ verify: VerifyDiscordRequest(process.env.PUBLIC_KEY) }));
 
 // Store for in-progress games. In production, you'd want to use a DB
-const activeGames = {};
 
 /**
  * Interactions endpoint URL where Discord will send HTTP requests
@@ -45,87 +44,9 @@ app.post('/interactions', async function (req, res) {
   if (type === InteractionType.APPLICATION_COMMAND) {
     const { name } = data;
     var command = cmdList.find(c => c.name==name)
-    console.log("command: "+command)
+    console.log("command: "+JSON.stringify(command))
     command.func(req, res, id)
   }
-  
-  // if (type === InteractionType.MESSAGE_COMPONENT) {
-  // // custom_id set in payload when sending message component
-  // const componentId = data.custom_id;
-
-  //   if (componentId.startsWith('accept_button_')) {
-  //     // get the associated game ID
-  //     const gameId = componentId.replace('accept_button_', '');
-  //     // Delete message with token in request body
-  //     const endpoint = `webhooks/${process.env.APP_ID}/${req.body.token}/messages/${req.body.message.id}`;
-  //     try {
-  //       await res.send({
-  //         type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
-  //         data: {
-  //           // Fetches a random emoji to send from a helper function
-  //           content: 'What is your object of choice?',
-  //           // Indicates it'll be an ephemeral message
-  //           flags: InteractionResponseFlags.EPHEMERAL,
-  //           components: [
-  //             {
-  //               type: MessageComponentTypes.ACTION_ROW,
-  //               components: [
-  //                 {
-  //                   type: MessageComponentTypes.STRING_SELECT,
-  //                   // Append game ID
-  //                   custom_id: `select_choice_${gameId}`,
-  //                   options: getShuffledOptions(),
-  //                 },
-  //               ],
-  //             },
-  //           ],
-  //         },
-  //       });
-  //       // Delete previous message
-  //       await DiscordRequest(endpoint, { method: 'DELETE' });
-  //     } catch (err) {
-  //       console.error('Error sending message:', err);
-  //     }
-  //   } else if (componentId.startsWith('select_choice_')) {
-  //     // get the associated game ID
-  //     const gameId = componentId.replace('select_choice_', '');
-
-  //     if (activeGames[gameId]) {
-  //       // Get user ID and object choice for responding user
-  //       const userId = req.body.member.user.id;
-  //       const objectName = data.values[0];
-  //       // Calculate result from helper function
-  //       const resultStr = getResult(activeGames[gameId], {
-  //         id: userId,
-  //         objectName,
-  //       });
-
-  //       // Remove game from storage
-  //       delete activeGames[gameId];
-  //       // Update message with token in request body
-  //       const endpoint = `webhooks/${process.env.APP_ID}/${req.body.token}/messages/${req.body.message.id}`;
-
-  //       try {
-  //         // Send results
-  //         await res.send({
-  //           type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
-  //           data: { content: resultStr },
-  //         });
-  //         // Update ephemeral message
-  //         await DiscordRequest(endpoint, {
-  //           method: 'PATCH',
-  //           body: {
-  //             content: 'Nice choice ' + getRandomEmoji(),
-  //             components: []
-  //           }
-  //         });
-  //       } catch (err) {
-  //         console.error('Error sending message:', err);
-  //       }
-  //     }
-  //   }
-  // }
-  
 });
 
 app.listen(PORT, () => {
